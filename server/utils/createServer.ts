2397607801970c5f8ae0b54776e412f416e7cf45
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-09-16 16:59:41 +0100

    basic server is set up and running

--- server/utils/createServer.ts
+++ server/utils/createServer.ts
@@ -1,4 +1,30 @@
+import { buildSchema } from 'type-graphql';
+import UserResolver from '../src/modules/user/userResolver';
+import { ApolloServer } from 'apollo-server-fastify';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import fastify from 'fastify';
+
+// initialising app
+const app = fastify();
+
+const buildContext = () => {};
+
+// creating a server
 export const createServer = async () => {
+  const schema = await buildSchema({
+    resolvers: [UserResolver],
+  });
 
+  const server = new ApolloServer({
+    schema,
+    context: buildContext,
+    // defining plugins
+    plugins: [
+      ApolloServerPluginDrainHttpServer({
+        httpServer: app.server,
+      }),
+    ],
+  });
 
+  return { app, server };
 };
